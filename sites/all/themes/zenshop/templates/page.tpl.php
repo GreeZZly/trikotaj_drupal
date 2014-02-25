6ff5f99a0918fdd1a01e7265875ee8c9cf95196f
Egor Kosolapov <greezzly7@gmail.com>
2014-02-25 17:45:45 +0400

    half front page styled

--- sites/all/themes/zenshop/templates/page.tpl.php
+++ sites/all/themes/zenshop/templates/page.tpl.php
@@ -19,81 +19,71 @@
 
 <div id="page">
 
-  <header class="header" id="header" role="banner">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 class="header__site-name" id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
-        <?php endif; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-
-  </header>
-  
-
-
-  <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-	  
-	    
-	  <div class="pkvbgrd">
-		<?php //надо так 
-		print render($page['pkv_region']); 
-		?>
-	  </div>
-	  
-	  
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+  <div id="header_background">
+    <div id="header">
+      <ul>
+        <li>
+          <div id="header_left">
+            <?php if ($logo): ?>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+              <?php endif; ?>
+            <div id="search">
+            
+            <?print render($page['header_left']);?>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div id="header_center">
+            <div id="header_phone">+7(8352)63-06-54</div>
+            <div id="header_socials">
+              <div class="icon_header_vk"></div>
+              <div class="icon_header_tweet"></div>
+              <div class="icon_header_fb"></div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div id="header_right">
+            <div id="logistr">
+             <!--  <div id="registr">Регистрация</div>
+              <div> | </div>
+              <div id="login">Вход</div> -->
+              <?php if ($secondary_menu): ?>
+                <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+                  <?php print theme('links__system_secondary_menu', array(
+                    'links' => $secondary_menu,
+                    'attributes' => array(
+                      'class' => array('links', 'inline', 'clearfix'),
+                    ),
+                    'heading' => array(
+                      'text' => $secondary_menu_heading,
+                      'level' => 'h2',
+                      'class' => array('element-invisible'),
+                    ),
+                  )); ?>
+                </nav>
+              <?php endif; ?>
+            </div>
+            <div id="cart">
+              <div id="cart_title">Корзина</div>
+              <div id="cart_items">Товары: <b>0</b></div>
+              <div id="cart_summ">Сумма: <b>0</b></div>
+            </div>
+            <?php print render($page['header_right']); ?>
+          </div>
+        </li>
+        <li class="helper"></li>
+      </ul>
+      <?php print render($page['header']); ?>
     </div>
+  </div>
+  
+<!-- НАВИГАЦИОННЫЙ БАР -->
 
-    <div id="navigation">
-
-      <?php if ($main_menu): ?>
+<div id="navbar_background" class="block_background">
+  <div  class="center_block" id="navbar">
+    <?php if ($main_menu): ?>
         <nav id="main-menu" role="navigation" tabindex="-1">
           <?php
           // This code snippet is hard to modify. We recommend turning off the
@@ -113,15 +103,71 @@
           )); ?>
         </nav>
       <?php endif; ?>
+    <?php print render($page['navigation']); ?>
+  </div>
+</div>
 
-      <?php print render($page['navigation']); ?>
+<!-- НАВИГАЦИОННЫЙ БАР. КОНЕЦ -->
 
+<!-- РЕКЛАМНЫЙ БЛОК -->
+<div id="commercial_background">
+  <div class="center_block" id="commercial">
+    <div>
+    
+
+      <?php print render($page['hero']); ?>
     </div>
+  </div>
+</div>
+<div id="pusto_mesto_cvetno"></div>
+<!-- РЕКЛАМНЫЙ БЛОК. КОНЕЦ -->
+
+<!-- ОТЗЫВЫ  -->
+<div id="reviews_background">
+  <div id="reviews" class="center_block">
+      <?php //надо так 
+    print render($page['pkv_region']); 
+    ?>
+    <!-- <div class="block_title">Отзывы</div>
+    <ul>
+      <li>
+        <div class="reviews_item">
+          <div class="reviews_item_pic"><img src="/sites/all/themes/zenshop/include/images/rev1.png"></div>
+          <div class="reviewer_name">Настя</div>
+          <div class="review_item_text">"Выбор просто замечательный! Для прогулки по городу, встречи в кафе с друзьями... Я нашла миллион идей для своего образа. Мама тоже очень..."</div>
+        </div>
+      </li>
+      <li>
+        <div class="reviews_item">
+          <div class="reviews_item_pic"><img src="/sites/all/themes/zenshop/include/images/rev2.png"></div>
+          <div class="reviewer_name">Марина</div>
+          <div class="review_item_text">"Выбор этого магазина дает мне быть разной и неповторимой. На долгожданный вечер с любимым человеком просто необходимо выбрать..."</div>
+        </div>
+      </li>
+      <li>
+        <div class="reviews_item">
+          <div class="reviews_item_pic"><img src="/sites/all/themes/zenshop/include/images/rev3.png"></div>
+          <div class="reviewer_name">Катя</div>
+          <div class="review_item_text">"Для учебы подойдет удобная и строгая одежда. А ведь еще нужно решить, в чем пойти на вечеринку! Здесь я нашла для себя то, что искала для создания..."</div>
+        </div>
+      </li>
+      <li class="helper"></li>
+    </ul> -->
+  </div>
+</div>
+<!-- ОТЗЫВЫ КОНЕЦ -->
+
+<!-- СОЦИАЛЬНЫЕ КНОПКИ -->
+<div id="soclikes_background">
+  <div class="center_block" id="soclikes">
+    
+      <?print render($page['soclikes']);?>
+  </div>
+</div>
+<!-- СОЦИАЛЬНЫЕ КНОПКИ КОНЕЦ -->
+  <div id="main">
+
 	
-	
-
-
-
     <?php
       // Render the sidebars to see if there's anything in them.
       $sidebar_first  = render($page['sidebar_first']);
@@ -144,21 +190,21 @@
 <?php print render($page['bottom']); ?>
 
 <?php //так не надо 
-  include_once("main/htmlheader.html");
-  include_once("main/header.php");
-  include_once("main/navbar.php");
-  include_once('main/commercial.php');
-  include_once('main/reviews.php');
-  include_once('main/soclikes.php');
-  include_once('main/likebox.php');
-  include_once('main/banner.php');
-  include_once('main/mostsale.php');
-  include_once('main/banner1.php');
-  include_once('main/newprod.php');
-  include_once('main/counter.php');
-  include_once('main/minimap.php');
-  include_once('main/footer.php');
-  include_once("main/htmlfooter.html");
+  // include_once("main/htmlheader.html");
+  // include_once("main/header.php");
+  // include_once("main/navbar.php");
+  // include_once('main/commercial.php');
+  // include_once('main/reviews.php');
+  // include_once('main/soclikes.php');
+  // include_once('main/likebox.php');
+  // include_once('main/banner.php');
+  // include_once('main/mostsale.php');
+  // include_once('main/banner1.php');
+  // include_once('main/newprod.php');
+  // include_once('main/counter.php');
+  // include_once('main/minimap.php');
+  // include_once('main/footer.php');
+  // include_once("main/htmlfooter.html");
 
 
 ?>
