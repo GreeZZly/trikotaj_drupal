9dd5e4033762b89eae92729add9d5384315c7f29
Egor Kosolapov <greezzly7@gmail.com>
2014-03-01 16:26:24 +0400

    cart, pages etc

--- sites/all/themes/zenshop/templates/page.tpl.php
+++ sites/all/themes/zenshop/templates/page.tpl.php
@@ -125,6 +125,53 @@
 <div id="pusto_mesto_cvetno"></div>
 <!-- РЕКЛАМНЫЙ БЛОК. КОНЕЦ -->
 
+
+<!-- контент  -->
+<div id="main_background">
+  <div id="main">
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+     
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+    
+ 
+    
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div>
+
+   
+  
+
+
+
+    <?php
+      // Render the sidebars to see if there's anything in them.
+      $sidebar_first  = render($page['sidebar_first']);
+      $sidebar_second = render($page['sidebar_second']);
+    ?>
+
+    <?php if ($sidebar_first || $sidebar_second): ?>
+      <aside class="sidebars">
+        <?php print $sidebar_first; ?>
+        <?php print $sidebar_second; ?>
+      </aside>
+    <?php endif; ?>
+
+  </div>
+</div>
+<!-- контент  -->  
+
 <!-- ОТЗЫВЫ  -->
 <div id="reviews_background">
   <div id="reviews" class="center_block">
@@ -161,12 +208,14 @@
 <!-- ОТЗЫВЫ КОНЕЦ -->
 
 <!-- СОЦИАЛЬНЫЕ КНОПКИ -->
+<?php if (!empty($page['soclikes'])){?>
 <div id="soclikes_background">
   <div class="center_block" id="soclikes">
     
       <?print render($page['soclikes']);?>
   </div>
 </div>
+<?php }?>
 <!-- СОЦИАЛЬНЫЕ КНОПКИ КОНЕЦ -->
 
 <!-- LIKEBOX -->
